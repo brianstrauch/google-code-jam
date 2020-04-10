28b65de271337e5a897846681a745e155b58186d
Brian Strauch <bstrauch24@gmail.com>
2020-04-09 19:47:26 -0500

    Template

---
+++ 2020/1a/b.py
@@ -0,0 +1,12 @@
+def read_int():
+    return int(input())
+
+def read_ints():
+    return [int(x) for x in input().split()]
+
+def solve():
+    pass
+
+tt = read_int()
+for cc in range(1, tt + 1):
+    print('Case #{}:'.format(cc))
